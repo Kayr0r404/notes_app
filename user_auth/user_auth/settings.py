6fc827734352f9b2b3efd48a6d7b870dd7ce8ed6
Karabo Mahlare <123119832+Kayr0r404@users.noreply.github.com>
2025-08-08 22:30:49 +0200

    Update settings.py

--- user_auth/user_auth/settings.py
+++ user_auth/user_auth/settings.py
@@ -148,5 +148,5 @@ AUTH_USER_MODEL = "users.CustomUser"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://notes-app-6hhk.onrender.com/api/token/",  # React default port
+    "https://notes-app-6hhk.onrender.com:8000",  # React default port
 ]
